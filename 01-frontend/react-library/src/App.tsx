673471b9a4c135100e5030b71c9625249ac792f8
ibraisi <iskiibra@hotmail.com>
2022-11-26 01:27:31 +0100

    added carouselComponent

--- 01-frontend/react-library/src/App.tsx
+++ 01-frontend/react-library/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './Layouts/NavbarAndFooter/Navbar';
-import { ExploreTopBooks } from './Layouts/NavbarAndFooter/HomePage/ExploreTopBooks';
+import { ExploreTopBooks } from './Layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './Layouts/HomePage/Carousel';
 
 function App() {
   return (
     <div>
     <Navbar/>
     <ExploreTopBooks/>
+    <Carousel/>
     </div>
   );
 }
---
+++ 01-frontend/react-library/src/Layouts/HomePage/Carousel.tsx
@@ -0,0 +1,121 @@
+export const Carousel = () => {
+  return (
+    <div className="container mt-5" style={{ height: 550 }}>
+      <div className="homepage-carousel-title">
+        <h3>Find your next "I stayed up too late reading" book.</h3>
+      </div>
+      <div
+        id="carouselExampleControls"
+        className="carousel carousel-dark slide mt-5 
+                    d-none d-lg-block"
+        data-bs-interval="false"
+      >
+        {/* Desktop */}
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div className="row d-flex justify-content-center align-items-center">
+              <div className="col-xs-6 col-sm-6 col-lg-3 mb-3"></div>
+              <div className="text-center">
+                <img
+                  src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+                  alt="book"
+                  height="233"
+                  width="133"
+                />
+                <h6 className="mt-2">Book</h6>
+                <p className="">luv2code</p>
+                <a href="#" className="btn main-color text-white">
+                  Reservce
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="carousel-item active">
+            <div className="row d-flex justify-content-center align-items-center">
+              <div className="col-xs-6 col-sm-6 col-lg-3 mb-3"></div>
+              <div className="text-center">
+                <img
+                  src={require("./../../Images/BooksImages/new-book-1.png")}
+                  alt="book"
+                  height="233"
+                  width="133"
+                />
+                <h6 className="mt-2">Book</h6>
+                <p className="">luv2code</p>
+                <a href="#" className="btn main-color text-white">
+                  Reservce
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="carousel-item active">
+            <div className="row d-flex justify-content-center align-items-center">
+              <div className="col-xs-6 col-sm-6 col-lg-3 mb-3"></div>
+              <div className="text-center">
+                <img
+                  src={require("./../../Images/BooksImages/new-book-2.png")}
+                  alt="book"
+                  height="233"
+                  width="133"
+                />
+                <h6 className="mt-2">Book</h6>
+                <p className="">luv2code</p>
+                <a href="#" className="btn main-color text-white">
+                  Reservce
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+      {/* Mobile */}
+      <div className="d-lg-none mt-3">
+        <div className="row d-flex justify-content-center align-items-center">
+          <div className="text-center">
+            <img
+              src={require("./../../Images/BooksImages/new-book-2.png")}
+              alt="book"
+              height="233"
+              width="133"
+            />
+            <h6 className="mt-2">Book</h6>
+            <p className="">luv2code</p>
+            <a href="#" className="btn main-color text-white">
+              Reservce
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="homepage-carousel-title mt-3">
+        <a className="btn btn-outline-secondary btn-lg" href="#">
+          View More
+        </a>
+      </div>
+    </div>
+  );
+};
